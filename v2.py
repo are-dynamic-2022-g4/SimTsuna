c522a04e960ce312b73683f32b1374253982efb2
Mika Vogel <101204555+Mikavgl@users.noreply.github.com>
2022-04-19 00:24:42 +0200

    Add files via upload

--- v2.py
+++ v2.py
@@ -17,7 +17,6 @@ résistance matériau:
     https://www.dynacast.com/fr-fr/ressources/dynamic-process-metal-selector
     https://www.holcimpartner.ch/fr/betonpraxis/proprietes-mecaniques-du-beton-durci
     
-
 """
 from math import ceil
 import random
@@ -29,7 +28,7 @@ class Batiment:
         self.hauteur=hauteur #25-150
         self.etat=etat #100-0
         self.coords=coords #(i,j)
-        self.mat=ceil(resistance/2)
+        self.mat=ceil(resistance/2) #1-5
         
     """def set_value_resi(self, valeur:int): #si besoin de set une value
         self.resi = valeur"""
@@ -69,18 +68,17 @@ class Vague:
         return "% s % s % s % s" % (self.force, self.hauteur, self.pression, self.coords)
 
 class Civil:
-    def __init__(self, nb_total, nb_blesse, coords):
+    def __init__(self, nb_total, coords):
         self.nb_total = nb_total
-        self.nb_blesse = nb_blesse
         self.coords=coords
         
     def __repr__(self):
-        return "% s % s % s" % (self.nb_total, self.nb_blesse, self.coords)
+        return "% s % s" % (self.nb_total, self.coords)
 
 class Protection:
-    def __init__(self, types, niv_protection):
+    def __init__(self, types, niv_protection, coords):
         self.types = types #1-3 0=detruit
-        self.niv_protection = niv_protection #1-5 0=detruit
+        self.niv_protection = niv_protection #1-5 0=detruit # sert à connaitre le matériau
         if types==0:
             self.hauteur = 0
         if types==1:
@@ -90,6 +88,16 @@ class Protection:
         elif types==3:
             self.hauteur = niv_protection*3
         self.mat = niv_protection
+        self.coords = coords
+        
+    def detruit(self):
+        self.types=0
+        self.niv_protection=0
+        self.hauteur=0
+        self.mat = 0
+        
+    def __repr__(self):
+        return "% s % s % s % s % s" % (self.types, self.niv_protection, self.hauteur, self.mat, self.coords)
         
 def show(matrice):
     for i in matrice:
@@ -122,9 +130,9 @@ def crea_batiment(ville, taille):
     taille_ville = int((taille/3)*2)
     for i in range(taille):
         for j in range(taille_ville):
-            bat = [Batiment(random.randint(1, 10),random.randint(25,150),100,(i,j))]
+            bat = [Batiment(random.randint(1, 10),random.randint(15,100),100,(i,j))]
             ville[i][j] = bat
-        ville[i][taille_ville-1] = [Batiment(0,0,100,(i,taille_ville-1))]
+        ville[i][taille_ville-1] = '_'
     return ville
 
 def crea_vague(ville, force, taille):
@@ -137,21 +145,12 @@ def crea_vague(ville, force, taille):
                 ville[i][j] = [Vague(force, (i,j))]
     return ville
 
-def liste_voisins(coord:tuple, taille: int) -> list:
-    """
-    Prends en paramètre un couple de coordonnées et une taille et retourne tous les voisins des coordonnées.
-    """
-    i, j = coord
-    return [(i+k, j+l) for k in range(-1,2) for l in range(-1,2)
-            if not ((l==k) and (l==0)) and i+k >= 0 and i+k < taille
-            and j+l >=0 and j+l < taille]
-
-def liste_all(taille):
-    lst=[]
+def crea_protection(ville, taille, types, niv_protection):
     for i in range(taille):
         for j in range(taille):
-           lst.append((i,j))
-    return lst
+            if ville[i][j]=='_':
+                ville[i][j]=[Protection(types, niv_protection, (i,j))]
+    return ville
     
 def civilisation(ville, taille, densite):
     """ 
@@ -166,83 +165,123 @@ def civilisation(ville, taille, densite):
                 ht = ville[i][j][0].hauteur
                 nb_civil = round(densite*ht)
                 nb_civil_total = nb_civil_total + nb_civil 
-                mat_civil[i][j] = [Civil(nb_civil, 0, (i,j))]
+                mat_civil[i][j] = [Civil(nb_civil, (i,j))]
             else:
-                mat_civil[i][j] = [Civil(0, 0, (i,j))]
+                mat_civil[i][j] = [Civil(0, (i,j))]
     return mat_civil, nb_civil_total
 
-def budget_ville(ville, taille, lst_prix):
+def budget_ville(ville, taille, lst_prix): #eh merce hugo
     budget_ville=0
     for i in range(taille):
         for j in range(taille):
             if isinstance(ville[i][j][0],Batiment)==True:
                 etage = int(ville[i][j][0].hauteur/3.3)
-                budget_bat = lst_prix[ville[i][j][0].resi]*etage*100
+                budget_bat = lst_prix[ville[i][j][0].resi]*etage*100 #car surface==100m2 ?
                 budget_ville+= budget_bat
             elif isinstance(ville[i][j][0], Protection)==True:
-                print("pas encore fait...")
-    print(" ")
+                budget_protection = lst_prix[ville[i][j][0].mat*2]*ville[i][j][0].hauteur*50
+                budget_ville += budget_protection
+                #faire deux budget différents ? 
     return budget_ville
                 
-def tsunami(ville, taille, mat_civil, nb_civil_total):
-    f_v = 0
-    h_v = 0   
+def tsunami(ville, taille, mat_civil):
+    h_v = 0
     p_v = 0
+    nb_civil_total = 0
     for i in range(taille):
         for j in reversed(range(taille)):
-            temp.append((i,j))
+            #♥print(h_v, p_v) #debuggage
             if isinstance(ville[i][j][0],Vague)==True: #mise a jour de la force et la hauteur de la vague
                 h_v = ville[i][j][0].hauteur
                 p_v = ville[i][j][0].pression
             if isinstance(ville[i][j][0], Batiment)==True: #calcul de la collision
                 if ville[i][j][0].hauteur*1.1<h_v: #marge de 10%
                     ville[i][j][0].detruit()
-                    nb_civil_total -= mat_civil[i][j][0].nb_total
                     mat_civil[i][j][0].nb_total = 0
                     h_v = h_v*0.9
                     p_v = p_v*0.9
-                elif ville[i][j][0].hauteur*1.1>=h_v and ville[i][j][0].hauteur*0.9<=h_v:
-                    #calcul si le batiment fait environ la meme taille que la vague
+                elif ville[i][j][0].hauteur*1.1>=h_v:
                     #dépend donc des "stats" de celui-ci
-                elif ville[i][j][0].hauteur*0.9<h_v:
-                    #calcul si la vague est peu "dangereuse" pour la batiment
-                    
+                    if p_v>lst_resi[ville[i][j][0].mat]:
+                        temp = p_v - lst_resi[ville[i][j][0].mat] #si la vague est + puissante
+                        pourcentage = (100*temp/p_v)
+                        h_v = h_v - (h_v*pourcentage)/100
+                        p_v = temp
+                        ville[i][j][0].etat = int(ville[i][j][0].etat - (ville[i][j][0].etat*pourcentage)/100)
+                        mat_civil[i][j][0].nb_total = int((pourcentage*mat_civil[i][j][0].nb_total)/100)
+                        nb_civil_total += mat_civil[i][j][0].nb_total
+                    if p_v<lst_resi[ville[i][j][0].mat]:
+                        temp = lst_resi[ville[i][j][0].mat] - p_v #si le mur résiste 
+                        pourcentage = (100*temp)/lst_resi[ville[i][j][0].mat]
+                        ville[i][j][0].resi =  int(ville[i][j][0].resi - (ville[i][j][0].resi*pourcentage)/100)
+                        h_v = (h_v*pourcentage)/100
+                        p_v = (pourcentage*p_v)/100
+                        ville[i][j][0].etat = int(ville[i][j][0].etat - (ville[i][j][0].etat*pourcentage)/100)
+                        mat_civil[i][j][0].nb_total = int((pourcentage*mat_civil[i][j][0].nb_total)/100)
+                        nb_civil_total += mat_civil[i][j][0].nb_total
             if isinstance(ville[i][j][0], Protection)==True: #calcul collision avec les protections
-                #à faire
-    return ville
+                if ville[i][j][0].hauteur*1.1<h_v:
+                    ville[i][j][0].detruit()
+                elif ville[i][j][0].hauteur*1.1>h_v:
+                    if p_v>lst_resi[ville[i][j][0].mat]:
+                        temp = p_v - lst_resi[ville[i][j][0].niv_protection]
+                        pourcentage = (100*temp/p_v)
+                        h_v = h_v - (h_v*pourcentage)/100
+                        p_v = temp
+                    if p_v<lst_resi[ville[i][j][0].mat]:
+                        temp = lst_resi[ville[i][j][0].niv_protection] - p_v #si le mur résiste 
+                        pourcentage = (100*temp)/lst_resi[ville[i][j][0].niv_protection]
+                        ville[i][j][0].niv_protection =  int(ville[i][j][0].niv_protection - (ville[i][j][0].niv_protection*pourcentage)/100)
+                        h_v = (h_v*pourcentage)/100
+                        p_v = (pourcentage*p_v)/100
+    return ville, mat_civil, nb_civil_total
 
+def main():
+    #mise en place des listes pour la suite
+    lst_densite = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
+    lst_prix = [1000, 1150, 1300, 1450, 1600, 1750, 1900, 2050, 2200, 2350, 2500]
+    lst_materiaux = ["détruit", "brique", "bois", "béton", "roche", "béton fibré"] #[20,30,50,150,250]
+    lst_resi = [0, 20, 30, 50, 150, 250]
+    lst_protection = ["détruit", "brise-lames", "digue", "mur"]
+    
+    #mise en place de la ville
+    taille = int(input("Taille matrice de la ville: "))
+    force = int(input("Force du tsunami (d'après l'échelle d'Imamura: "))
+    ville = crea_ville(taille)
+    ville = crea_batiment(ville, taille)
+    ville = crea_vague(ville, force, taille)  
+    
+    #mise en place civil & budget
+    densite = int(input('Niveau de densité de population: '))
+    mat_civil, nb_civil_total = civilisation(ville, taille, lst_densite[densite])
+    return 
 ###############################################################################
 #On set toutes les variables utiles pour les tests
 lst_densite = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
-lst_prix = [1000, 1150, 1300, 1450, 1600, 1750, 1900, 2050, 2200, 2350]
+lst_prix = [1000, 1150, 1300, 1450, 1600, 1750, 1900, 2050, 2200, 2350, 2500]
 lst_materiaux = ["détruit", "brique", "bois", "béton", "roche", "béton fibré"] #[20,30,50,150,250]
+lst_resi = [0, 20, 30, 50, 150, 250]
 lst_protection = ["détruit", "brise-lames", "digue", "mur"]
-taille = 5
+taille = 7
+force = 4
 
 ville_test = crea_ville(taille)
-print("Ville vide")
-show(ville_test) #test création d'une matrice vide pour la ville
-
 ville_test = crea_batiment(ville_test,taille)
-print("Ville avec batiment et plage")
-show(ville_test) #test creation de la ville
-
-force = 4
 ville_test = crea_vague(ville_test, force, taille)
-print("Ville complete (ville + plage + vague)")
-show(ville_test) #test creation de la vague
 
-temp = []
-lst_all = liste_all(taille)
 mat_civil, nb_civil_total = civilisation(ville_test, taille, lst_densite[0])
 print("Civils dans la ville:", nb_civil_total)
 show(mat_civil) #test matrice des civils
 
-budget_ville = budget_ville(ville_test, taille, lst_prix)
-print("Le budget pour cette ville est: ", budget_ville) #test fn du budget (faite par Hugo)
+budget = budget_ville(ville_test, taille, lst_prix)
+print("Le budget pour cette ville est: ", budget, "€") #test fn du budget (faite par Hugo)
 
-show_mat(ville_test, (0,0)) #test verif d'un matériau
+ville_test = crea_protection(ville_test, taille, 1, 5)
+budget2 = budget_ville(ville_test, taille, lst_prix)
+print("Le budget pour cette ville+protection est: ", budget2, "€")
 
-#ville_test = tsunami(ville_test, taille, mat_civil, nb_civil_total)
+show(ville_test)
+ville_test, mat_civil, new_nb_civil_total = tsunami(ville_test, taille, mat_civil)
 print("Ville après tsunami")
+print("Civils dans la ville:", new_nb_civil_total, ", Nombre de pertes :", nb_civil_total-new_nb_civil_total)
 show(ville_test) #test des collisions du tsunami (fn coeur)
